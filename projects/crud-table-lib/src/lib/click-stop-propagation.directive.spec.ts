25511d5d6a5ef566e06042451da5e36f9e486cca
Kiss, Aron <aron123dev@gmail.com>
2019-05-05 14:30:43 +0200

    Add test of ClickStopPropagationDirective

--- projects/crud-table-lib/src/lib/click-stop-propagation.directive.spec.ts
+++ projects/crud-table-lib/src/lib/click-stop-propagation.directive.spec.ts
@@ -1,8 +1,20 @@
 import { ClickStopPropagationDirective } from './click-stop-propagation.directive';
 
 describe('ClickStopPropagationDirective', () => {
+
+  let directive;
+
   it('should create an instance', () => {
-    const directive = new ClickStopPropagationDirective();
+    directive = new ClickStopPropagationDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('should stop propagation of event', () => {
+    const event = new Event('click');
+    const stopPropagationSpy = spyOn(event, 'stopPropagation');
+
+    directive.onClick(event);
+
+    expect(stopPropagationSpy).toHaveBeenCalled();
   });
 });
