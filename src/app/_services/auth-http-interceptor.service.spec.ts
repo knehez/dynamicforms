d1ad48b92604846093f8a9d97c6ad40ae9ef33ec
Kiss, Aron <aron123dev@gmail.com>
2019-05-05 16:39:08 +0200

    Add tests for AuthHttpInterceptorService

--- src/app/_services/auth-http-interceptor.service.spec.ts
+++ src/app/_services/auth-http-interceptor.service.spec.ts
@@ -1,19 +1,82 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthHttpInterceptorService } from './auth-http-interceptor.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from './authentication.service';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+
+const TEST_ACCESS_TOKEN = 'exampleAccessToken';
+const TEST_BEARER_TOKEN_STRING = `Bearer ${TEST_ACCESS_TOKEN}`;
+const TEST_API_ENDPOINT = '/backend/example';
+
+class AuthenticationServiceMock {
+  getToken () {
+    return TEST_ACCESS_TOKEN;
+  }
+}
 
 describe('AuthHttpInterceptorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientModule,
-      RouterTestingModule
-    ]
-  }));
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
+  let interceptor: AuthHttpInterceptorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        AuthHttpInterceptorService,
+        {
+          provide: AuthenticationService,
+          useValue: new AuthenticationServiceMock()
+        },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthHttpInterceptorService,
+          multi: true
+        }
+      ]
+    });
+
+    httpClient = TestBed.get(HttpClient);
+    httpMock = TestBed.get(HttpTestingController);
+    interceptor = TestBed.get(AuthHttpInterceptorService);
+  });
 
   it('should be created', () => {
-    const service: AuthHttpInterceptorService = TestBed.get(AuthHttpInterceptorService);
-    expect(service).toBeTruthy();
+    expect(interceptor).toBeDefined();
+  });
+
+  it('should put Authorization header on requests', () => {
+    httpClient.get(TEST_API_ENDPOINT).subscribe(res => {
+      expect(res).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(TEST_API_ENDPOINT);
+    expect(httpRequest.request.headers.has('Authorization')).toBe(true);
+  });
+
+  it('should set Authorization header\'s value in \'Bearer <access-token>\' format', () => {
+    httpClient.get(TEST_API_ENDPOINT).subscribe(res => {
+      expect(res).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(TEST_API_ENDPOINT);
+    expect(httpRequest.request.headers.get('Authorization')).toBe(TEST_BEARER_TOKEN_STRING);
+  });
+
+  it('should use access token from AuthenticationService', () => {
+    httpClient.get(TEST_API_ENDPOINT).subscribe(res => {
+      expect(res).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(TEST_API_ENDPOINT);
+    const actualToken = httpRequest.request.headers.get('Authorization').split(' ')[1];
+    const expectedToken = TEST_ACCESS_TOKEN;
+
+    expect(actualToken).toBe(expectedToken);
   });
 });
