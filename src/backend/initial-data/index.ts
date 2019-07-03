e46e7d11fba94a3a066e6c72a80610c63274e7b1
Kiss, Aron <aron123dev@gmail.com>
2019-07-03 12:35:34 +0200

    Fix initialization: insert synchronously

--- src/backend/initial-data/index.ts
+++ src/backend/initial-data/index.ts
@@ -2,13 +2,13 @@ import UserInitializer from './userInitializer';
 import RoleInitializer from './roleInitializer';
 import { Initializer } from './initializer';
 
-export default function initializeDatabase () {
+export default async function initializeDatabase () {
     const initializers: Initializer[] = [
         new RoleInitializer,
         new UserInitializer
     ];
 
     for (const initializer of initializers) {
-        initializer.initialize();
+        await initializer.initialize();
     }
 }
