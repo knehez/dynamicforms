0fe90cd749aa690ef76c056cfd6bb8b63cd15b9b
knehez <karoly.nehez@uni-miskolc.hu>
2025-09-20 15:49:17 +0200

    angular 9 upgrade

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -79,6 +79,8 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+import '@angular/localize/init';
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
